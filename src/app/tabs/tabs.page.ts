840a56e78997cc817777ae09f8a09b20db2a1d8d
Gerardo Peralta Cebrián <gerardo_cebrian@hotmail.com>
2026-05-04 14:23:13 -0600

    modified:   package-lock.json 	deleted:    src/app/app.component.spec.ts 	deleted:    src/app/explore-container/explore-container.component.spec.ts 	deleted:    src/app/pages/seleccion-establecimiento/seleccion-establecimiento.page.spec.ts 	deleted:    src/app/tab1/tab1.page.spec.ts 	deleted:    src/app/tab2/tab2.page.spec.ts 	deleted:    src/app/tabs/tabs.page.spec.ts 	modified:   src/app/tabs/tabs.page.ts 	modified:   tsconfig.app.json 	modified:   tsconfig.json

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,18 +1,19 @@
 import { Component, EnvironmentInjector, inject } from '@angular/core';
-import { IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel } from '@ionic/angular/standalone';
+import {  IonTabs, IonIcon, IonTabBar, IonTabButton, IonLabel } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
-import { triangle, ellipse, square } from 'ionicons/icons';
+import { triangle, ellipse, square, home, fastFoodOutline, person, cart } from 'ionicons/icons';
 
 @Component({
   selector: 'app-tabs',
   templateUrl: 'tabs.page.html',
   styleUrls: ['tabs.page.scss'],
-  imports: [IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel],
+  standalone: true,
+  imports: [IonLabel, IonTabButton, IonTabBar, IonTabs, IonIcon],
 })
 export class TabsPage {
   public environmentInjector = inject(EnvironmentInjector);
 
   constructor() {
-    addIcons({ triangle, ellipse, square });
+    addIcons({home,fastFoodOutline,person,cart,triangle,ellipse,square});
   }
 }
